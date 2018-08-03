967b23853635f68866e9bd6de7a56b056d8b710b
Barby87 <bcaceres.sepulveda@hotmail.com>
2018-08-02 22:17:07 -0300

    ejercicio 1 de arrays findShortestOfThreeWords

---
+++ arrays/findShortestOfThreeWords.js
@@ -0,0 +1,12 @@
+function findShortestOfThreeWords(word1, word2, word3) {
+ 
+  if (word1.length < word2.length < word3.length) {
+     return word1;
+   }
+ 
+  if (word1.length === word2.length === word3.length) {
+     return word1;  
+   }
+ 
+ }
+ 
