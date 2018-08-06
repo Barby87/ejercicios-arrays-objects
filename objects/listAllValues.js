95e63be0713a785156921048d62128ae67cd1dbe
Barby87 <bcaceres.sepulveda@hotmail.com>
2018-08-06 14:30:40 -0300

    Ejercicio 2 de Objects listAllValues

---
+++ objects/listAllValues.js
@@ -0,0 +1,9 @@
+// Escribe una función llamada listAllValues que devuelve un array de todos los valores del objeto de entrada.
+const listAllValues = (obj) => {
+
+  let objectWithValues = Object.values(obj);
+  //El método Object.values() devuelve un array con los valores de las propiedades del objeto dado.
+  return objectWithValues;
+}
+ 
+
